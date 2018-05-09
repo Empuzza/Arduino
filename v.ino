672a3a3e08635c8ea4d0deb4128a5362dfc1f11a
Empuzza <37378499+Empuzza@users.noreply.github.com>
2018-05-09 18:45:08 +0200

    Add files via upload

---
+++ v.ino
@@ -0,0 +1,19 @@
+int diodaPin = 13;
+int Pot;
+int PotPin = 0;
+int przerwa;
+
+void setup() {
+  pinMode (diodaPin, OUTPUT);
+  
+}
+
+void loop() {
+  Pot = analogRead(PotPin)
+  przerwa = map(Pot, 0, 1023, 50, 3000)
+  digitalWrite(diodaPin, HIGH);
+  delay(przerwa);             
+  digitalWrite(diodaPin, LOW);
+  delay(przerwa);             
+
+}
